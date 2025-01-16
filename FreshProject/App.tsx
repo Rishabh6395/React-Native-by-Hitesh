1904cb340f29df6f1bd27e1d228bc4085c58fef0
Rishabh <rishabhbhatt956@gmail.com>
2025-01-16 08:36:22 +0530

    Add password generation functionality with state management

--- FreshProject/App.tsx
+++ FreshProject/App.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 // Form validation -- this is for from validation
 import * as Yup from 'yup'
@@ -12,6 +12,32 @@ const PasswordSchema = Yup.object().shape({
 })
 
 export default function App() {
+
+  const [password, setPassword] = useState('')
+  const [isPasswordGenerated, setIsPasswordGenerated] = useState(false)
+
+  const [lowerCase, setLowerCase] = useState(true)
+  const [upperCase, setUpperCase] = useState(false)
+  const [numbers, useNumbers] = useState(false)
+  const [symbols, useSymbol] = useState(false)
+
+  const generatePasswordString = (passwordLength: number) =>{
+
+  }
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = ''
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length)
+      result += characters.charAt(characterIndex)
+    }
+    return result
+  }
+
+  const resetPasswordState = () => {
+
+  }
+
   return (
     <View>
       <Text>App</Text>
